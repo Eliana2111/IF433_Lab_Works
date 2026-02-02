a61d10f931938b312542e467e1fd2cd5310f448b
Felicia Aulia <your-email@example.com>
2026-02-02 16:48:02 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_136309_FeliciaAuliaSuherman/week1/GameStore.kt
+++ src/oop_136309_FeliciaAuliaSuherman/week1/GameStore.kt
@@ -1,6 +1,17 @@
 package oop_136309_FeliciaAuliaSuherman.week1
 
-fun main {
+fun main () {
    val gameTitle: String = "Valorant"
    val price: Int = 450000
+
+    println("Game Title: $gameTitle")
+    calculatediscount(price)
+}
+
+fun calculatediscount(price: Int){
+      if(price < 500000) {
+         println("Price: ${price - (price * 0.1)}")
+   }  else {
+         println("Price: ${price - (price * 0.2)}")
+   }
 }
