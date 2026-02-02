942228517d40c7afffd935af96a38b4b11624ae4
Felicia Aulia <your-email@example.com>
2026-02-02 15:40:00 +0700

    weel01: initial setup and procedural code

--- src/oop_136309_FeliciaAuliaSuherman/week1/GradingSystem.kt
+++ src/oop_136309_FeliciaAuliaSuherman/week1/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_136309_FeliciaAuliaSuherman.week1
 
 fun main() {
-    var name: String = "Felicia"
-    var score: Int = 85
+    l name: String = "Felicia"
+    val score: Int = 85
 
     println("Nama: " + name + ", Nilai: " + score)
 }
