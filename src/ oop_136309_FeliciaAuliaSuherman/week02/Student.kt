34a072885eea812eb1d607582f36f883a0bde4ad
felic <feliciaulia24@gmail.com>
2026-02-09 15:24:44 +0700

    week02: add secondary constructor

--- src/ oop_136309_FeliciaAuliaSuherman/week02/Student.kt
+++ src/ oop_136309_FeliciaAuliaSuherman/week02/Student.kt
@@ -9,4 +9,7 @@ class Student (val name: String, val nim: String, var major: String, ) {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
     }
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructur jalur umum (Tanpa Jurusan).")
+    }
 }
