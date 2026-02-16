2c58e1b1e3a3e9874ba16291f9b27064672a6908
Eliana2111 <feliciaulia24@gmail.com>
2026-02-16 15:43:53 +0700

    week03: simulate stack overflow error in setter

--- src/oop_136309_FeliciaAulia/week03/Employee.kt
+++ src/oop_136309_FeliciaAulia/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_136309_FeliciaAulia.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_136309_FeliciaAulia/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_136309_FeliciaAulia.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
