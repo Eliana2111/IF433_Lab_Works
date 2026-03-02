be6b686d17f9d455969fed111d40ea083f8a5ab5
felic <feliciaulia24@gmail.com>
2026-03-02 14:05:31 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_136309_FeliciaAulia/week05/tugas02/EWallet.kt
@@ -0,0 +1,18 @@
+package oop_136309_FeliciaAulia.week05.tugas02
+
+class EWallet(var balance: Double) : PaymentMethod(accountName = "EWallet") {
+    override fun processPayment(amount : Double) {
+        if (balance > amount) {
+            balance -= amount
+            println("Pembayaran berhasil sebesar $amount. Sisa saldo: $balance")
+        }
+        else {
+            println("Saldo tidak cukup.")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil sebesar $amount. Saldo sekarang: $balance")
+    }
+}
