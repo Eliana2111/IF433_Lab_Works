6cc437f6ea994bf77c56cbcdc4d5ed72cda81877
felic <feliciaulia24@gmail.com>
2026-03-02 14:53:29 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_136309_FeliciaAulia/week05/tugas02/Main.kt
+++ src/oop_136309_FeliciaAulia/week05/tugas02/Main.kt
@@ -8,6 +8,13 @@ fun main() {
     for (method in paymentMethod) {
         println("=== Pembayaran menggunakan ${method.accountName} ===")
         method.processPayment(75000.0)
+        if (method is EWallet) {
+            println("Saldo kurang, program akan otomatis melakukan top up.")
+            method.topUp(50000.0)
+
+            println("Mencoba ulang transaksi.")
+            method.processPayment(75000.0)
+        }
         println()
     }
 }
