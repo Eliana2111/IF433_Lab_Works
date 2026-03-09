cb9716ddcc8483d86ae95bf91e908182a90d3f71
Theovillus <brayzso416@gmail.com>
2026-03-09 15:50:33 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_136309_FeliciaAulia/week06/SmartSpeaker.kt
@@ -0,0 +1,15 @@
+package oop_136309_FeliciaAulia.week06
+
+class SmartSpeaker (override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("Smart Speaker $name dinyalakan.")
+    }
+
+    override fun turnOff() {
+        println("Smart Speaker $name dimatikan.")
+    }
+
+    fun playMusic(song: String){
+        println("Memutar lagu $song dari Spotify")
+    }
+}
