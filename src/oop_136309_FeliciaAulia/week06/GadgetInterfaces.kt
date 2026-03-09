b18a78688c71733b3d31104b1714e52e941a4e9c
Theovillus <brayzso416@gmail.com>
2026-03-09 15:22:56 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_136309_FeliciaAulia/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_136309_FeliciaAulia.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
