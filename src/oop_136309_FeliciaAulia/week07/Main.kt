4c8fc1d3b7d1f75cb4674955340689e390d3da46
felic <feliciaulia24@gmail.com>
2026-03-16 14:16:24 +0700

    week07: implement data class for structural equality

--- src/oop_136309_FeliciaAulia/week07/Main.kt
+++ src/oop_136309_FeliciaAulia/week07/Main.kt
@@ -6,12 +6,12 @@ fun main() {
     DatabaseManager.connect()
 
     println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient() // di commit sebelumnya lupa di perbaiki tapi commit ini udh di perbaiki kok ka maap hehe
+    val client = NetworkClient.createClient()
     client.connect()
 
     println("\n=== TEST REGULAR CLASS ===")
-    val req1 = RegularUser("Alice", 22)
-    val req2 = RegularUser("Alice", 22)
-    println(req1)
-    println("Sama? ${req1 == req2}")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1)
+    println("Sama? ${data1 == data2}")
 }
--- src/oop_136309_FeliciaAulia/week07/UserModels.kt
+++ src/oop_136309_FeliciaAulia/week07/UserModels.kt
@@ -1,3 +1,4 @@
 package oop_136309_FeliciaAulia.week07
 
-class RegularUser(val name: String, val age: Int)
+data class DataUser(val name: String, val age: Int)
+
