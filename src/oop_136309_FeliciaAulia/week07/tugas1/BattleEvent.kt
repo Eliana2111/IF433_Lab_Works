d8ea124a77abe8d6ccede561dbe3fe3d4bc356b0
felic <feliciaulia24@gmail.com>
2026-03-16 19:10:33 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_136309_FeliciaAulia/week07/tugas1/BattleEvent.kt
@@ -0,0 +1,8 @@
+package oop_136309_FeliciaAulia.week07.tugas1
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState()
+}
