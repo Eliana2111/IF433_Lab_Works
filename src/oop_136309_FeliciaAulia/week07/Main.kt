9a5018d000ea56d04d78d8f49bc51d043ca06e10
felic <feliciaulia24@gmail.com>
2026-03-16 14:14:03 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_136309_FeliciaAulia/week07/Main.kt
+++ src/oop_136309_FeliciaAulia/week07/Main.kt
@@ -6,6 +6,12 @@ fun main() {
     DatabaseManager.connect()
 
     println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient("https:api.umn.ac.id")
+    val client = NetworkClient.createClient() // di commit sebelumnya lupa di perbaiki tapi commit ini udh di perbaiki kok ka maap hehe
     client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val req1 = RegularUser("Alice", 22)
+    val req2 = RegularUser("Alice", 22)
+    println(req1)
+    println("Sama? ${req1 == req2}")
 }
---
+++ src/oop_136309_FeliciaAulia/week07/UserModels.kt
@@ -0,0 +1,3 @@
+package oop_136309_FeliciaAulia.week07
+
+class RegularUser(val name: String, val age: Int)
