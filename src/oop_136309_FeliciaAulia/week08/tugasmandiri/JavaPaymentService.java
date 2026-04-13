b1a810689b3bdeef648fb04c3ff6ae4ca741fd92
Eliana2111 <feliciaulia24@gmail.com>
2026-04-13 15:48:16 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_136309_FeliciaAulia/week08/tugasmandiri/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_136309_FeliciaAulia.week08.tugasmandiri;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
