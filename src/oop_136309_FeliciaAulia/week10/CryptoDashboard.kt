0835b50ba36c326f7065c8c72c4c98931dea0de9
felic <feliciaulia24@gmail.com>
2026-04-27 13:17:43 +0700

    week10: (task) populate coin repository

--- src/oop_136309_FeliciaAulia/week10/CryptoDashboard.kt
+++ src/oop_136309_FeliciaAulia/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_136309_FeliciaAulia.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.75))
+    coinRepo.add(Coin("ETH", 2.50))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
